09b744db09632cf6889941298cbd6c76475774b0
LeoRDG <34048656+LeoRDG@users.noreply.github.com>
2025-08-18 19:37:53 -0300

    Add files via upload

---
+++ presencial/primeiro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	
+	int numero;
+	
+	cout << "Digite um numero: ";
+	
+	cin >> numero;
+	
+	cout << "Numero Informado: " << numero << endl;
+	
+	return 0;
+}
+
